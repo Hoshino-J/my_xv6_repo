44b361c9cd535dd647f2a715b00e9d9d3c9b2c46
Stone749990226 <749990226@qq.com>
2024-09-30 20:21:34 +0800

    feat(.gitingnore): 将commit.patch加入.gitignore中

---
+++ user/sleep.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "user.h"
+
+int main(int argc,char* argv[]){
+    if(argc != 2){
+        printf("Sleep needs one argument!\n"); 
+        exit(-1);
+    }
+    int ticks = atoi(argv[1]); 
+    sleep(ticks);             
+    printf("(nothing happens for a little while)\n");
+    exit(0); 
+}
